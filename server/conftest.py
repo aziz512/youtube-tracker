fe0ad786164bcc7ebfb796de335790203bf15738
edmond120 <edmondwong888@msn.com>
2022-04-04 22:08:28 -0400

    Server: Add new dependency feedparser and tests

--- server/conftest.py
+++ server/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import feedparser
 from app import create_app
 
 @pytest.fixture()
@@ -9,3 +10,14 @@ def app():
 @pytest.fixture()
 def client(app):
 	return app.test_client()
+
+@pytest.fixture()
+def feed():
+	"""
+	Test data is from the official Youtube channel
+	https://www.youtube.com/feeds/videos.xml?channel_id=UCBR8-60-B28hp2BmDPdntcQ
+
+	This channel features videos from other channels as well as their
+	own. This makes it useful for testing edge cases.
+	"""
+	return feedparser.parse('tests/testdata/rss_feed.txt')
