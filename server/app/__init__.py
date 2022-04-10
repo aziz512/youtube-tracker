35daa4824fc59be2f68c395038f1cc39b717df56
edmond120 <edmondwong888@msn.com>
2022-04-10 11:48:46 -0400

    Server: add omissions in coverage report

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,7 +6,7 @@ def create_app(rss_watchlist=None):
 	app = Flask(__name__)
 
 	# rss_watchlist is a path to a file
-	if rss_watchlist is None:
+	if rss_watchlist is None: # pragma: no cover
 		app.config['watchlist'] = 'watchlist'
 	else:
 		app.config['watchlist'] = rss_watchlist
