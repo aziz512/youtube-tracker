4028b106be1b9ceb8179eba59ab12486eac5a7ab
edmond120 <edmondwong888@msn.com>
2022-04-28 21:20:58 -0400

    Server: make threading async

--- server/app/video_download.py
+++ server/app/video_download.py
@@ -1,4 +1,4 @@
-import threading
+import asyncio
 from yt_dlp import YoutubeDL
 from yt_dlp.utils import DownloadError
 
@@ -11,7 +11,7 @@ class DownloadVideo:
 		ydl_opts = self.ydl_opts
 		with YoutubeDL(ydl_opts) as ydl:
 			try:
-				threading.Thread(target=ydl.download([URL]))
+				await asyncio.to_thread(lambda: ydl.download([URL]))
 			except DownloadError:
 				return "DownloadError"
 		return "Done"
